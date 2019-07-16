a54a7fdc286f65f8dcf32c0fcce46d9c407a821e
Stefano Silvestrini <silver.stefano@gmail.com>
2019-07-16 05:17:45 +0200

    middleware of cors and body-parser inserted in index.js for later

--- index.js
+++ index.js
@@ -2,10 +2,16 @@ const express = require('express')
 const path = require('path')
 const bodyParser = require('body-parser')
 const cors = require('cors')
-
 require('dotenv').config()
 
 const app = express()
+
+//It gives access to the body of the request that we can send to the database by Postman
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
+
+app.use(cors({allowHeaders: true}))
+
 //Api
 
 
