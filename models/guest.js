80358ddb707c8a19720e4bdd64a711bfc01a302f
Stefano Silvestrini <silver.stefano@gmail.com>
2019-07-16 09:45:49 +0200

    model guest created

---
+++ models/guest.js
@@ -0,0 +1,14 @@
+const db = require('../db')
+
+const db_guest = db.model('guest', {
+	points: {
+		type: Number,
+		default: 0
+	},
+	rounds: {
+		type: Number,
+		default: 0
+	}
+})
+
+module.exports = db_guest
