e18a7bb02532382c0fd35dc856aac25e29b6822e
Stefano Silvestrini <silver.stefano@gmail.com>
2019-07-22 08:57:55 +0200

    fucntion to calculate points for logged users

--- controllers/post_checkAnswer.js
+++ controllers/post_checkAnswer.js
@@ -1,14 +1,47 @@
 const db_user = require('../models/user')
 const jwt = require('jsonwebtoken')
+const axios = require('axios')
 
 module.exports = (req, res) => {
-	axios.get(`https://api.datamuse.com/words${endpoint}`).then((res) => {
+	axios.get(`https://api.datamuse.com/words${req.body.endpoint}`).then((res) => {
 		if(res.data && res.data.length){
-			console.log(res.data);
-			console.log('hello')
+			let answer = req.body.answer.toLowerCase()
+			let correct_answers = res.data.map( (c) => c.word )
+			console.log('correct answers' , correct_answers);
+			let correct_answer = correct_answers.find( (element) => {
+				return element === answer
+			})
+			let streak = req.body.streak
+			let points_added = 2^streak
+			let token = req.headers.authorization.split(' ')[1]
+			if (token) {
+				// verify
+				jwt.verify(token, process.env.SECRET, (err, decoded) => {
+					if(decoded) {
+						// find user
+						db_user.findById(decoded._id).then((user) => {
+							if(correct_answer) {
+								user.points += points_added
+								streak++
+							} else {
+								user.points -= 1
+								streak = 0
+							}
+							// Update user
+							db_user.findByIdAndUpdate(decoded._id, user).then((u) => {
+								console.log('u', u);
+							})
+						}).catch( (err) =>{
+							console.log(err);
+						})
+
+					}
+
+				})
+			}
+
 		}
+	}).catch( (err) => {
+		console.log(err);
 	})
-	console.log('req body' , req.body);
-	console.log('req headers' , req.headers);
-	// console.log('res' ,res.data);
 }
