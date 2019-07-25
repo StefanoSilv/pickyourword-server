2625ad3d46f7cf54b0155920b4cd5de41af2b089
Stefano Silvestrini <silver.stefano@gmail.com>
2019-07-25 11:53:53 +0200

    controller pay edited, bugs fixed

--- controllers/pay.js
+++ controllers/pay.js
@@ -1,13 +1,25 @@
 const stripe = require('stripe')(process.env.STRIPE_SECRET)
 
 module.exports = (req, res) => {
-	console.log('req.body', req.body)
 	stripe.charges.create({
-		amount: req.body.amount * 100,
+		amount: 2.99 * 100,
 		currency: 'usd',
 		source: req.body.token
 	}).then((data) => {
 		res.send(data)
+		let token = req.headers.authorization.split(' ')[1]
+		jwt.verify(token, process.env.SECRET, (err, decoded) => {
+			if (decoded) {
+				db_user.findById(decoded._id).then( (user) => {
+					user.user_type = 'premium'
+					db_user.findByIdAndUpdate(decoded._id, user, {new: true}).then( (u) => {
+						res.json( u )
+					})
+				}).catch((err) => {
+					res.send(err)
+				})
+			}
+		})
 	}).catch((err) => {
 		console.log('err', err)
 		res.send(err)
