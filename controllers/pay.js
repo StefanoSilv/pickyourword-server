39dc598b3a3525397fcad4769d645fceafd73bbe
Stefano Silvestrini <silver.stefano@gmail.com>
2019-07-29 04:55:55 +0200

    stripe working in the backend

--- controllers/pay.js
+++ controllers/pay.js
@@ -1,27 +1,32 @@
 const stripe = require('stripe')(process.env.STRIPE_SECRET)
+const bcrypt = require('bcrypt')
+const jwt = require('jsonwebtoken')
+const db_user = require('../models/user')
+
 
 module.exports = (req, res) => {
-	console.log('req', req.body);
-	console.log('source',req.body.token);
 	stripe.charges.create({
 		amount: 2.99 * 100,
 		currency: 'usd',
-		source: req.body.token
-	}).then((data) => {
-		console.log(data);
-		res.send(data)
+		source: req.body.token.id
+	}).then( (data) => {
 		let token = req.headers.authorization.split(' ')[1]
 		jwt.verify(token, process.env.SECRET, (err, decoded) => {
 			if (decoded) {
 				db_user.findById(decoded._id).then( (user) => {
-					console.log('user type',user.user_type);
 					user.user_type = 'premium'
 					db_user.findByIdAndUpdate(decoded._id, user, {new: true}).then( (u) => {
-						res.json( u )
-						console.log(u);
+						console.log(typeof(data));
+						console.log(typeof(u));
+						console.log('user',u);
+						res.status(200).json({
+							user: u,
+							stripe: data
+						})
 					})
 				}).catch((err) => {
-					res.send(err)
+					console.log(err);
+					// res.send(err)
 				})
 			}
 		})
